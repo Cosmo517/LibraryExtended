2062ce778fc85e215f42d794f5902ae3077973ae
Naibitx <naibys.alzugarayofficial@gmail.com>
2024-06-21 20:30:20 -0400

    FrontEnd Login and Register Pages

---
+++ FrontEnd/LibraryExtended/src/Pages/Login.jsx
@@ -0,0 +1,133 @@
+import React, {useState} from 'react';
+import api from '../api';
+import Cookies from 'js-cookie';
+import '../CSS/login.css';
+
+const GoogleFontsStyle = `
+    @import url('https://fonts.googleapis.com/css2?family=Space+Mono:wght@700&display=swap');
+`;
+
+export const Login = () => {
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
+    })
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        // grab the info element for error messages
+        let info = document.getElementById('info')
+        // create a signed JWT on user login
+        let signed_response = await api.post('/login/', formData)
+        setFormData({
+            username: '',
+            password: ''
+        })
+
+        if (signed_response['data']['token'])
+        {
+            // Store the token as a cookie and reroute to the dashboard
+            Cookies.set('token', signed_response['data']['token'])
+            window.location.href='/';
+        }
+        else 
+        {
+            info.innerHTML = 'Invalid username or password'
+        }
+    };
+
+
+/*export const Login = () => {
+    const[formData, setFormData] = useState({
+        username: '',
+        password: '',
+    })
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        const info = document.getElementById('info');
+
+        try{
+            const signed_response = await api.post('/login/', formData);
+            const token = signed_response.data.token;
+
+            if (token) {
+                Cookies.set('token', token)
+                window.location.href = '/';
+            }
+            else{
+                info.innerHTML = 'Invalid username or password';
+            }
+
+            setFormData({username: '', password: ' '});
+        } 
+        catch(error){
+            info.innerHTML = 'Error while logging in';
+            console.error('Login Error:', error);
+        }
+
+    };*/
+    const handleInputChange = (event) => {
+        const value = event.target.value
+        setFormData({
+            ...formData,
+            [event.target.name]: value,
+        });
+    };
+    //const handleInputChange = (event) => {
+       // const{ name, value } = event.target;
+        //setFormData((prevFormData) => ({
+         //   ...prevFormData,
+            //[name]: value,
+       // }));
+   // };
+
+    return (
+
+        <div className='wrap'>
+            <div className={'login-form-box space-mono-bold'}>
+                <h3>
+                    Login
+                </h3>
+                <form onSubmit= {handleFormSubmit}>
+                    <div className="input-box">
+                        <span className="icon"><ion-icon name="person"></ion-icon> </span>
+                        <input 
+                            type='text'
+                            name='Username'
+                            id='username'
+                            required
+                            onChange={handleInputChange}
+                            value={formData.username}
+                        />
+                    </div>
+                    <label>Username</label>
+                    <div className='input-box'>
+                        <span className="icon"><ion-icon name="lock-closed"></ion-icon></span>
+                        <input 
+                            type='Password'
+                            id='password'
+                            name='Password'
+                            required
+                            onChange={handleInputChange}
+                            value={formData.password}
+                            
+                        />
+                    </div>
+                    <label>Password</label>
+                    <button type = 'submit' class='bttn space-mono-bold'>Login</button>
+                    <div className='account-register'>
+                        <a href="/#register">Register an account</a>
+                    </div>
+                    <div id="info"></div>
+                </form>
+            </div>
+        </div>
+        
+    );
+
+
+
+};
+
+export default Login;
---
+++ FrontEnd/LibraryExtended/src/Pages/Register.jsx
@@ -0,0 +1,75 @@
+import React, {useState} from 'react'
+import api from '../api'
+
+export const Register = () => {
+    const [username, setUsername] = useState ('');
+    const [password, setPassword] = useState ('');
+    const [email, setEmail] = useState('');
+    const [admin, setAdmin] = useState (0);
+
+    const handleInputChange = (event) => {
+        const {name, value} = event.target;
+        if(name==='username') setUsername(value);
+        else if (name==='password') setPassword(value);
+        else if (name==='email') setEmail(value);
+        else if (name==='admin') setAdmin(perseInt(value,10));
+    };
+
+    const validateEmail=(email) => {
+        const valid= /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
+        return valid.test(email);
+    };
+
+    const displayMessage = (message) => {
+        document.getElementById('info').innerHTML= message;
+    };
+    const handleFormSubmit = async(event) => {
+        event.preventDefault();
+        try{
+            if(
+                username.trim()===''||
+                username.length>25 ||
+                password.length<10 ||
+                !validateEmail(email)
+            ) {
+                if(username.trim()===''){
+                    displayMessage('Username is blank');
+                }
+                else if(username.length > 25){
+                    displayMessage('Username must be 25 character or less');
+                }
+                else if (!validateEmail(email)){
+                    displayMessage('invalid email address');
+                }
+                else if (password.length < 10){
+                    displayMessage('Password must be 10 character or longer');
+                }
+            }
+            else{
+                const response = await api.post('/users/',{
+                    username,
+                    password,
+                    email,
+                    admin
+                });
+                if (repsponse.data.reponse==='Success'){
+                    setUsername('');
+                    setPassword('');
+                    setEmail('');
+                    setAdmin(0);
+                    displayMessage('Account has been created successfully');
+                }
+                else if(response.data.response === 'Username taken'){
+                    displayMessage('Username already exist');
+                }
+                else {
+                    displayMessage('Server error')
+                }
+            }
+        } catch (error){
+            console.error('Error:', error);
+            displayMessage('Error');
+        }
+    };
+}
+
